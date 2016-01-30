d4d25189cfcd416e2ccc2b0bdc844c4ffdfb5fc6
FabioFleitas <fabio@tesorio.com>
2016-01-29 19:02:15 -0800

    Added spamcheck.py

---
+++ spamcheck.py
@@ -0,0 +1,28 @@
+import requests
+
+API_URL = 'http://spamcheck.postmarkapp.com/filter'
+
+
+def _parse_response(response):
+    content = response.json()
+
+    if response.status_code != 200 or not content.get('success'):
+        error_msg = content.get('message', 'API Error')
+        raise Exception(error_msg)
+
+    return content
+
+
+def check(email, report=False):
+    data = {
+        'email': email,
+        'options': 'long' if report else 'short'
+    }
+    headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.post(API_URL, data, headers=headers)
+
+    return _parse_response(response)
